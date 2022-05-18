44fb7deeb8f829c4808461b014a34fb03bbfd47a
antipod237 <antipod2370@gmail.com>
2022-05-18 17:43:40 +0300

    added suppliers model

--- backend/erpsystem/core/models.py
+++ backend/erpsystem/core/models.py
@@ -1,10 +1,12 @@
-from .roles.models import RoleModel
-from .users.models import UserModel
-from .permissions.models import PermissionModel, PermissionAction
+from  erpsystem.core.roles.models import RoleModel
+from erpsystem.core.users.models import UserModel
+from erpsystem.core.permissions.models import PermissionModel, PermissionAction
+from erpsystem.core.suppliers.models import SupplierModel
 
 __all__ = [
     'UserModel',
     'RoleModel',
     'PermissionModel',
     'PermissionAction',
+    'SupplierModel',
 ]
