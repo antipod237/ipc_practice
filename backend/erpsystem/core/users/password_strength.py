1d95a5be0c0a77347d93f7f00e72264ba514be7c
AbsolutelyH <lvutin.76@mail.ru>
2022-05-19 22:49:29 +0300

    Add password valid fixed2.

--- backend/erpsystem/core/users/password_strength.py
+++ backend/erpsystem/core/users/password_strength.py
@@ -9,7 +9,7 @@ class PasswordPolicy():
          l = len(password)
          u = sum(i.isupper() for i in password)
          n = sum(i.isdigit() for i in password)
-         s = l - n -sum(i.isalpha () for i in password)
+         s = l - n - sum(i.isalpha () for i in password)
          if (l >= self.length & u >= self.uppercase & n >= self.numbers & s >= self.special): 
             return True
          return False
