8781ed94d1a11986fbafb558a2eb23427a1accda
antipod237 <antipod2370@gmail.com>
2022-05-21 02:04:17 +0300

    add contracts resources

--- backend/erpsystem/core/contracts/resources.py
+++ backend/erpsystem/core/contracts/resources.py
@@ -1,118 +1,90 @@
-import json
-from datetime import date
 from starlette.routing import Route
 from starlette.endpoints import HTTPEndpoint
 
 from ..database import db
-from ..models import ContractModel, PermissionAction
 from ..utils import (
     with_transaction, jwt_required, make_error,
     validation, GinoQueryHelper, Permissions,
-    make_list_response, make_response, NO_CONTENT, get_one,
+    make_list_response, make_response, NO_CONTENT,
+    get_one, get_date, 
 )
+from ..models import ContractModel, PermissionAction
 
 permissions = Permissions(app_name='contracts')
 
 
 class Contracts(HTTPEndpoint):
+
+    @staticmethod
     @jwt_required
     @permissions.required(action=PermissionAction.GET)
-    async def get(self, request):
-        data = request.query_params
-        contracts_query = ContractModel.query
+    async def get(request):
+        current_query = ContractModel.query
         total_query = db.select([db.func.count(ContractModel.id)])
 
-        if 'id' in data:
-            ids = json.loads(data['id'])
-            current_query, total_query = GinoQueryHelper.in_(
-                contracts_query,
-                total_query,
-                ContractModel.id,
-                ids
-            )
+        query_params = request.query_params
 
-        if 'number' in data:
-            contracts_query, total_query = GinoQueryHelper.search(
+        if 'search' in query_params:
+            current_query, total_query = GinoQueryHelper.search(
                 ContractModel.number,
-                data['number'],
-                contracts_query,
+                query_params['search'],
+                current_query,
                 total_query
             )
 
-        if 'start_date' in data:
-            year, month, day = data['start_date'].split('-')[:3]
-            contracts_query = contracts_query.where(
-                ContractModel.start_date >= date(year, month, day)
-            )
-            total_query = total_query.where(
-                ContractModel.start_date >= date(year, month, day)
-            )
-        if 'end_date' in data:
-            year, month, day = data['end_date'].split('-')[:3]
-            contracts_query = contracts_query.where(
-                ContractModel.start_date <= date(year, month, day)
-            )
-            total_query = total_query.where(
-                ContractModel.start_date <= date(year, month, day)
-            )
-
-        if 'supplier_id' in data:
-            # TODO replace with equal
-            contracts_query = contracts_query.where(
-                ContractModel.provider_id == int(data['supplier_id'])
-            )
-            total_query = total_query.where(
-                ContractModel.provider_id == int(data['supplier_id'])
-            )
-
-        contracts_query = GinoQueryHelper.pagination(
-            data, contracts_query
+        current_query = GinoQueryHelper.pagination(
+            query_params, current_query
         )
-
-        contracts_query = GinoQueryHelper.order(
-            data,
-            contracts_query,
-            {
+        current_query = GinoQueryHelper.order(
+            query_params,
+            current_query, {
                 'id': ContractModel.id,
                 'number': ContractModel.number,
                 'start_date': ContractModel.start_date,
-                'end_date': ContractModel.end_date,
+                'end_date':  ContractModel.end_date,
                 'supplier_id': ContractModel.supplier_id,
             }
         )
-
-        contracts = await contracts_query.gino.all()
+        print(ContractModel.start_date)
+        total = await total_query.gino.scalar()
+        items = await current_query.gino.all()
 
         return make_list_response(
-            [contract.jsonify() for contract in contracts],
-            total=await total_query.gino.scalar()
+            [item.jsonify() for item in items],
+            total,
         )
 
     @with_transaction
     @jwt_required
     @permissions.required(action=PermissionAction.CREATE)
-    async def post(self, request):
-
-        data = await request.json()
-
-        number = data['number']
-        start_date = self._get_date(data['start_date'])
-        end_date = self._get_date(data['end_date'])
-        supplier_id = data['supplier_id']
-
-        contract = await ContractModel.create(
-            number=number,
-            start_date=start_date,
-            end_date=end_date,
-            supplier_id=supplier_id,
+    @validation(schema={
+        'number': {
+            'required': True,
+            'type': str,
+            'min_length': 4,
+            'max_length': 50,
+        },
+        'start_date': {
+            'required': True,
+            'type': str,
+        },
+        'end_date': {
+            'required': True,
+            'type': str,
+        },
+        'supplier_id': {
+            'required': True,
+            'type': int,
+        },
+    })
+    async def post(self, data):
+        new_contract = await ContractModel.create(
+            number=data['number'],
+            start_date=get_date(data['start_date']),
+            end_date=get_date(data['end_date']),
+            supplier_id=data['supplier_id'],
         )
-
-        return make_response({'id': contract.id})
-
-    @staticmethod
-    def _get_date(text_date):
-        year, month, day = [int(i) for i in text_date.split('-')]
-        return date(year, month, day)
+        return make_response({'id': new_contract.id})
 
 
 class Contract(HTTPEndpoint):
@@ -131,17 +103,21 @@ class Contract(HTTPEndpoint):
     @permissions.required(action=PermissionAction.UPDATE)
     @validation(schema={
         'number': {
+            'required': True,
             'type': str,
-            'min_length': 1,
+            'min_length': 4,
             'max_length': 50,
         },
         'start_date': {
-            'type': date,
+            'required': True,
+            'type': str,
         },
         'end_date': {
-            'type': date,
+            'required': True,
+            'type': str,
         },
         'supplier_id': {
+            'required': True,
             'type': int,
         },
     }, return_request=True)
@@ -157,9 +133,9 @@ class Contract(HTTPEndpoint):
         values = {
             'number': data['number']
             if 'number' in data else None,
-            'start_date': data['start_date']
+            'start_date': get_date(data['start_date'])
             if 'start_date' in data else None,
-            'end_date': data['end_date']
+            'end_date': get_date(data['end_date'])
             if 'end_date' in data else None,
             'supplier_id': data['supplier_id']
             if 'supplier_id' in data else None,
@@ -171,7 +147,8 @@ class Contract(HTTPEndpoint):
 
         return NO_CONTENT
 
+
 routes = [
     Route('/', Contracts),
-    Route('/{contract_id:int}', Contract),
+    Route('/{id:int}', Contract),
 ]
--- backend/erpsystem/core/routes.py
+++ backend/erpsystem/core/routes.py
@@ -4,6 +4,7 @@ from starlette.responses import JSONResponse
 from .users.resources import routes as user_routes
 from .roles.resources import routes as roles_routes
 from .suppliers.resources import routes as suppliers_routes
+from .contracts.resources import routes as contracts_routes
 from .permissions.resources import get_apps
 
 
@@ -15,5 +16,6 @@ routes = [
     Route('/apps', get_apps, methods=['GET']),
     Mount('/users', routes=user_routes),
     Mount('/roles', routes=roles_routes),
-    Mount('/suppliers', routes=suppliers_routes)
+    Mount('/suppliers', routes=suppliers_routes),
+    Mount('/contracts', routes=contracts_routes)
 ]
