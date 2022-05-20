61abdcdc7463b021f9b934eb1bb158e763f4344d
antipod237 <antipod2370@gmail.com>
2022-05-21 02:09:23 +0300

    contracts model and migration

--- backend/erpsystem/core/contracts/models.py
+++ backend/erpsystem/core/contracts/models.py
@@ -1,4 +1,5 @@
 from ..database import db
+from ..utils.dates import convert_to_utc
 
 
 class ContractModel(db.Model):
@@ -6,8 +7,8 @@ class ContractModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     number = db.Column(db.String(50), nullable=False)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
+    start_date = db.Column(db.DateTime(timezone=True), nullable=False)
+    end_date = db.Column(db.DateTime(timezone=True), nullable=False)
     supplier_id = db.Column(
         db.Integer, db.ForeignKey('suppliers.id'), nullable=True
     )
@@ -16,13 +17,13 @@ class ContractModel(db.Model):
         return {
             'id': self.id,
             'number': self.number,
-            'start_date': self.start_date,
-            'end_date': self.end_date,
+            'start_date': convert_to_utc(self.start_date).isoformat(),
+            'end_date': convert_to_utc(self.end_date).isoformat(),
             'supplier_id': self.supplier_id,
         }
 
     @classmethod
     async def get_by_identifier(cls, identifier):
         return await cls.query.where(
-            (cls.id == identifier | cls.number == identifier)
+            (cls.id == identifier)
         ).gino.first()
