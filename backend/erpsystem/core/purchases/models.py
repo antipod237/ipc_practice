8c208b0067d9d834196e5a94128b4b59ee25baa6
antipod237 <antipod2370@gmail.com>
2022-05-31 16:57:09 +0300

    Revert "Revert "add purchase model""
    This reverts commit 84d79f326193f8dbfc68b6140047219e494935d7.

---
+++ backend/erpsystem/core/purchases/models.py
@@ -0,0 +1,82 @@
+from ..database import db
+
+
+class PurchaseModel(db.Model):
+    __tablename__ = 'purchases'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_set_id = db.Column(
+        db.Integer, db.ForeignKey('item_sets.id'), nullable=False
+    )
+    value = db.Column(db.Integer, nullable=False)
+    store_id = db.Column(
+        db.Integer, db.ForeignKey('stores.id'), nullable=False
+    )
+    date = db.Column(db.Date(), nullable=False)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False
+    )
+    is_complete = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._item_sets = None
+        self._stores = None
+        self._users = None
+
+    @property
+    def item_sets(self):
+        return self._item_sets
+
+    @property
+    def stores(self):
+        return self._stores
+
+    @property
+    def users(self):
+        return self._users
+
+    @item_sets.setter
+    def item_set(self, value):
+        self._item_set = value
+
+    @stores.setter
+    def store(self, value):
+        self._store = value
+
+    @users.setter
+    def user(self, value):
+        self._user = value
+
+    def jsonify(self, for_card=False):
+        result = {
+            'id': self.id,
+            'item_set': self.item_set.name,
+            'date': self.date.isoformat(),
+            'is_complete': self.is_complete
+        }
+
+        if for_card:
+            result['item_sets'] = [
+                {
+                    'item_set_id': item_set.id,
+                    'name': item_set.name
+                }
+                for item_set in self.item_set
+            ]
+            result['user'] = [
+                {
+                    'user_id': user.id,
+                    'username': user.display_name,
+                }
+                for user in self.users
+            ]
+            result['store'] = [
+                {
+                    'store_id': store.id,
+                    'store_address': store.address
+                }
+                for store in self.stores
+            ]
+
+        return result
