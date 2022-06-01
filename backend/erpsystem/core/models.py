e5a86b4d405e418691e2623ddc8f34f289adfaa7
SMDI17 <dima.smetanin-2012@yandex.ru>
2022-06-01 14:04:39 +0300

    Revert "add purchase model"
    This reverts commit 392bc51a41b0e154ba6e388e520778dc1cc64c1b.

--- backend/erpsystem/core/models.py
+++ backend/erpsystem/core/models.py
@@ -5,7 +5,6 @@ from .suppliers.models import SupplierModel
 from .contracts.models import ContractModel
 from .stores.models import StoresModel
 from .itemsets.models import ItemSetModel, ContractItemSetModel
-from .purchases.models import PurchaseModel
 
 __all__ = [
     'UserModel',
@@ -17,5 +16,4 @@ __all__ = [
     'StoresModel',
     'ItemSetModel',
     'ContractItemSetModel',
-    'PurchaseModel',
 ]
