c9cf0222c713dceec1a6e1b1a6539bad59c5ae3a
antipod237 <antipod2370@gmail.com>
2022-06-08 01:02:38 +0300

    update purchase model

--- backend/erpsystem/core/purchases/models.py
+++ backend/erpsystem/core/purchases/models.py
@@ -16,13 +16,13 @@ class PurchaseModel(db.Model):
     user_id = db.Column(
         db.Integer, db.ForeignKey('users.id'), nullable=False
     )
-    is_complete = db.Column(db.Boolean, nullable=False)
+    is_complete = db.Column(db.Boolean, default=False, nullable=False)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._item_sets = None
-        self._users = None
-        self._store_items = None
+        self._item_sets = set()
+        self._users = set()
+        self._store_items = set()
 
     @property
     def item_sets(self):
@@ -38,21 +38,22 @@ class PurchaseModel(db.Model):
 
     @item_sets.setter
     def item_set(self, value):
-        self._item_set = value
+        self._item_sets.add(value)
 
     @users.setter
     def user(self, value):
-        self._user = value
+        self._users.add(value)
 
     @store_items.setter
     def store_item(self, value):
-        self._store_items = value
+        self._store_items.add(value)
 
     def jsonify(self, for_card=False):
         result = {
             'id': self.id,
             'item_set_id': self.item_set_id,
             'store_item_id': self.store_item_id,
+            'value': self.value,
             'date': self.date.isoformat(),
             'is_complete': self.is_complete
         }
@@ -63,12 +64,13 @@ class PurchaseModel(db.Model):
                     'item_set_id': item_set.id,
                     'name': item_set.name
                 }
-                for item_set in self.item_set
+                for item_set in self.item_sets
             ]
             result['store_items'] = [
                 {
                     'store_item_id': store_item.id,
                     'name': store_item.name,
+                    'value': store_item.value
                 }
                 for store_item in self.store_items
             ]
