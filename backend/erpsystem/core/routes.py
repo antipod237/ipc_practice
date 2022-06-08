b405f8eda25d14b632c8f4297e26b728d6bb9175
Sergey Plotnikov <86552792+antipod237@users.noreply.github.com>
2022-06-09 01:07:13 +0300

    Update routes.py

--- backend/erpsystem/core/routes.py
+++ backend/erpsystem/core/routes.py
@@ -7,6 +7,7 @@ from .suppliers.resources import routes as suppliers_routes
 from .contracts.resources import routes as contracts_routes
 from .stores.resources import routes as stores_routes
 from .itemsets.resources import routes as item_sets_routes
+from .purchases.resources import routes as purchases_routes
 from .store_items.resources import routes as store_items_routes
 from .permissions.resources import get_apps
 
@@ -23,5 +24,6 @@ routes = [
     Mount('/contracts', routes=contracts_routes),
     Mount('/stores', routes=stores_routes),
     Mount('/itemsets', routes=item_sets_routes),
+    Mount('/purchases', routes=purchases_routes),
     Mount('/store_items', routes=store_items_routes),
 ]
