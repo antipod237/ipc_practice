4a2672012c35f91a22e23f393c863d4a20cfd59e
AbsolutelyH <lvutin.76@mail.ru>
2022-06-08 16:31:38 +0300

    store_items resources

--- backend/erpsystem/core/routes.py
+++ backend/erpsystem/core/routes.py
@@ -7,6 +7,7 @@ from .suppliers.resources import routes as suppliers_routes
 from .contracts.resources import routes as contracts_routes
 from .stores.resources import routes as stores_routes
 from .itemsets.resources import routes as item_sets_routes
+from .store_items.resources import routes as store_items_routes
 from .permissions.resources import get_apps
 
 
@@ -22,4 +23,5 @@ routes = [
     Mount('/contracts', routes=contracts_routes),
     Mount('/stores', routes=stores_routes),
     Mount('/itemsets', routes=item_sets_routes),
+    Mount('/store_items', routes=store_items_routes),
 ]
